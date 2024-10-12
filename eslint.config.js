a54037db05e5e42cb52290de2c2ad86a72f03e7f
EliNygard <eli.nygard@gmail.com>
2024-10-12 17:13:09 +0200

    update eslint plugin for jest

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,29 @@
 import globals from "globals";
-import eslintRecommended from "@eslint/js";
+import pluginJs from "@eslint/js";
+import pluginJest from "eslint-plugin-jest"; // Import the Jest plugin
 
 export default [
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,  // Set browser environment
-      },
-      ecmaVersion: "latest",  // Equivalent to es2021
-      sourceType: "module",   // Use ES modules
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser, // Set browser environment
+            },
+            ecmaVersion: "latest", // Equivalent to es2021
+            sourceType: "module", // Use ES modules
+        },
     },
-    rules: {
-      // Add custom rules here (empty for now)
+    pluginJs.configs.recommended, // Equivalent to "extends": "eslint:recommended"
+    {
+        files: ["**/*.test.js"], // Target test files
+        languageOptions: {
+            globals: globals.jest, // Set Jest environment
+        },
+        plugins: {
+            jest: pluginJest, // Add Jest plugin
+        },
+        rules: {
+            ...pluginJest.configs.recommended.rules, // Extend Jest recommended rules
+            "jest/prefer-expect-assertions": "off", // Custom Jest rule
+        },
     },
-  },
-  eslintRecommended.configs.recommended,  // Extend recommended ESLint config
 ];
