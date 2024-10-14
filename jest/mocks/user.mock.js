b1f5038a56b67416e363c677692b1e8023e77239
EliNygard <eli.nygard@gmail.com>
2024-10-14 21:45:22 +0200

    change name on mock profile const

--- jest/mocks/user.mock.js
+++ jest/mocks/user.mock.js
@@ -1,12 +1,12 @@
 // create a mock:
-const name = "TestUser";
-const email = "test@stud.noroff.no";
-const password = "passwordTest123";
-const accessToken = "tokenTest";
+const name = 'TestUser';
+const email = 'test@stud.noroff.no';
+const password = 'passwordTest123';
+const accessToken = 'tokenTest';
 
-export const mockUserProfile = {
-    name: name,
-    email: email,
-    password: password,
-    accessToken: accessToken,
+export const userProfileMock = {
+  name: name,
+  email: email,
+  password: password,
+  accessToken: accessToken,
 };
