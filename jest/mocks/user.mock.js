8e0af4719edc9edafc93391b75fa8415facb54d5
EliNygard <eli.nygard@gmail.com>
2024-10-14 12:24:41 +0200

    adds name to user mock

--- jest/mocks/user.mock.js
+++ jest/mocks/user.mock.js
@@ -1,9 +1,11 @@
 // create a mock:
+const name = "TestUser";
 const email = "test@stud.noroff.no";
 const password = "passwordTest123";
 const accessToken = "tokenTest";
 
 export const mockUserProfile = {
+    name: name,
     email: email,
     password: password,
     accessToken: accessToken,
