824a017fed145d5774ef2da0866c2048c7a0745d
EliNygard <eli.nygard@gmail.com>
2024-10-15 10:38:43 +0200

    run script task format

--- eslint.config.js
+++ eslint.config.js
@@ -1,55 +1,55 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginJest from "eslint-plugin-jest";
-import pluginCypress from "eslint-plugin-cypress";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginJest from 'eslint-plugin-jest';
+import pluginCypress from 'eslint-plugin-cypress';
 
 export default [
-    {
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-                ...globals.node,
-            },
-            ecmaVersion: "latest",
-            sourceType: "module",
-        },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
-    pluginJs.configs.recommended, // Equivalent to "extends": "eslint:recommended"
-    {
-        files: ["**/*.test.js"],
-        languageOptions: {
-            globals: globals.jest,
-        },
-        plugins: {
-            jest: pluginJest,
-        },
-        rules: {
-            ...pluginJest.configs.recommended.rules,
-            "jest/prefer-expect-assertions": "off",
-        },
+  },
+  pluginJs.configs.recommended, // Equivalent to "extends": "eslint:recommended"
+  {
+    files: ['**/*.test.js'],
+    languageOptions: {
+      globals: globals.jest,
     },
-    {
-        files: ["**/*.cy.js"],
-        languageOptions: {
-            globals: {
-                ...globals.cypress,
-            },
-        },
-        plugins: {
-            cypress: pluginCypress,
-        },
-        rules: {
-            ...pluginCypress.configs.recommended.rules,
-            "cypress/no-unnecessary-waiting": "off",
-            "no-unused-vars": "off",
-            "no-undef": "off",
-        },
+    plugins: {
+      jest: pluginJest,
     },
-    {
-        files: ["cypress.config.js", "**/*.js"],
-        rules: {
-            "no-unused-vars": "off",
-            "no-undef": "off",
-        },
+    rules: {
+      ...pluginJest.configs.recommended.rules,
+      'jest/prefer-expect-assertions': 'off',
     },
+  },
+  {
+    files: ['**/*.cy.js'],
+    languageOptions: {
+      globals: {
+        ...globals.cypress,
+      },
+    },
+    plugins: {
+      cypress: pluginCypress,
+    },
+    rules: {
+      ...pluginCypress.configs.recommended.rules,
+      'cypress/no-unnecessary-waiting': 'off',
+      'no-unused-vars': 'off',
+      'no-undef': 'off',
+    },
+  },
+  {
+    files: ['cypress.config.js', '**/*.js'],
+    rules: {
+      'no-unused-vars': 'off',
+      'no-undef': 'off',
+    },
+  },
 ];
