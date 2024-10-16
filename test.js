fe7c2371177351210e110703d461c33784aa0b30
EliNygard <eli.nygard@gmail.com>
2024-10-16 19:59:31 +0200

    config for env use, add simple test

---
+++ test.js
@@ -0,0 +1,10 @@
+import "dotenv/config";
+
+function test(expectedSecret) {
+  const secretMatches = process.env.TEST_USER_PASSWORD == expectedSecret;
+  console.log(`The meaning of life is ${process.env.TEST_USER_PASSWORD}`);
+  console.log(`The secret does${secretMatches ? "" : " not"} match!`);
+  return secretMatches;
+}
+
+test("testPassword123");
