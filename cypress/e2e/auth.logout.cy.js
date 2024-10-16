ed55183205f74b0e7b80bfd1b5366e027fbb6040
EliNygard <eli.nygard@gmail.com>
2024-10-17 01:13:07 +0200

    fix: move functions in test case, test pass

--- cypress/e2e/auth.logout.cy.js
+++ cypress/e2e/auth.logout.cy.js
@@ -1,13 +1,13 @@
 describe("Logout function", () => {
   beforeEach(() => {
     cy.visitHome();
-    cy.showLoginForm();
-    cy.loginWithTestUser();
-    cy.isLoggedIn();
   });
 
   it("the user should log out with the log out button", () => {
-    cy.get('button[data-auth="logout"]').click();
+    cy.showLoginForm();
+    cy.loginWithTestUser();
+    cy.isLoggedIn();
+    cy.get("button[data-auth=logout]").click();
 
     cy.isLoggedOut();
   });
