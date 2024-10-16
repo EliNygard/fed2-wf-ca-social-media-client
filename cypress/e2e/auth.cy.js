78be2270385c53c9b29eaed5d642f0df9bc4d2d6
EliNygard <eli.nygard@gmail.com>
2024-10-16 22:35:19 +0200

    rename and adds logout test case to hold all auth tests

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,23 +1,23 @@
-describe("Log in function", () => {
-  beforeEach(() => {
-    cy.visitHome();
-  });
-
+describe("Login and logout functions", () => {
   it("shows a register form", () => {
+    cy.visitHome();
     cy.get("#registerForm").should("be.visible");
   });
 
   it("shows a login form when the login button is clicked", () => {
+    cy.visitHome();
     cy.showLoginForm();
   });
 
   it("should allow a valid, registered user to log in with the login form", () => {
+    cy.visitHome();
     cy.showLoginForm();
     cy.loginWithTestUserWorks();
     cy.isLoggedIn();
   });
 
   it("should not allow a user to login with invalid credentials and is then shown a message", () => {
+    cy.visitHome();
     cy.showLoginForm();
 
     cy.login("invaliduser@example.no", "wrongPassword");
@@ -28,5 +28,16 @@ describe("Log in function", () => {
         "contain",
         "Either your username was not found or your password is incorrect",
       );
+  });
+
+  it("should allow the user to log out with the log out button", () => {
+    cy.visit(
+      "https://elinygard.github.io/fed2-wf-ca-social-media-client/?view=profile&name=TestUser",
+    );
+    cy.isLoggedOut();
+    cy.url().should(
+      "eq",
+      "https://elinygard.github.io/fed2-wf-ca-social-media-client/",
+    );
   });
 });
